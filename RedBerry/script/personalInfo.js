2ab8c5f4e7896b48854d2fd64d709d73da9fe231
BeqaGogberashvili <101138207+BeqaGogberashvili@users.noreply.github.com>
2022-03-08 06:21:04 +0400

    Add files via upload

--- RedBerry/script/personalInfo.js
+++ RedBerry/script/personalInfo.js
@@ -3,8 +3,7 @@ const check1 = localStorage.getItem("firstName");
 const check2 = localStorage.getItem("lastName");
 const check3 = localStorage.getItem("email");
 const check4 = localStorage.getItem("phone-mask");
-
-
+const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 function advance() {
   const firstName = document.querySelector("#firstName");
@@ -20,16 +19,18 @@ function advance() {
     const fn = document.getElementById("lastName");
     fn.classList.add("error");
   }
-  if(email.value.length < 3){
-    const fn = document.getElementById("email");
+if(!email.value.match(mailformat))
+{
+  const fn = document.getElementById("email");
     fn.classList.add("error");
-  }
+}
+  
   if(tel.value.length !== 13 || tel.value.charAt(0) != "+" || tel.value.charAt(1) != "9" || tel.value.charAt(2) != "9" || tel.value.charAt(3) != "5" ){
     const fn = document.getElementById("phone-mask");
     fn.classList.add("error");
   }
 
-  if( firstName && firstName.value.length > 2 && lastName && lastName.value.length > 2 && email && email.value.length > 2 && tel && tel.value.length > 2 && tel.value.length === 13 && (tel.value.charAt(0) == "+" && tel.value.charAt(1) == "9" && tel.value.charAt(2) == "9" && tel.value.charAt(3) == "5" )){
+  if( firstName && firstName.value.length > 2 && lastName && lastName.value.length > 2 && email && email.value.length > 2 && tel && tel.value.length > 2 && tel.value.length === 13 && (tel.value.charAt(0) == "+" && tel.value.charAt(1) == "9" && tel.value.charAt(2) == "9" && tel.value.charAt(3) == "5" && email.value.match(mailformat))){
       localStorage.setItem("firstName", firstName.value);
       localStorage.setItem("lastName", lastName.value);
       localStorage.setItem("email", email.value);
@@ -49,13 +50,6 @@ document.querySelector("#nextButton2").addEventListener("click", advance);
     document.getElementById("email").value = localStorage.getItem("email");
     document.getElementById("phone-mask").value = localStorage.getItem("phone-mask");
 
-    const one = document.getElementById("nextButton");
-    const two = document.getElementById("nextButton2");
-    one.addEventListener('click', function(){
-        location.replace("./technicalSkillset.html");
-    });
-    two.addEventListener('click', function(){
-        location.replace("./technicalSkillset.html");
-    });
+    
   }
 
